d4d70cf441cc00bdb6275548941eb4db76e6d3af
Scott Collins <sbc7556@gmail.com>
2015-11-17 13:39:57 -0600

    tidied up a few files

--- CMS/src/main/java/com/aegis/cms/dao/CmsUserDao.java
+++ CMS/src/main/java/com/aegis/cms/dao/CmsUserDao.java
@@ -1,17 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package com.aegis.cms.dao;
 
 import com.aegis.cms.model.User;
 import java.util.List;
 
-/**
- *
- * @author apprentice
- */
 public interface CmsUserDao {
 
     public void addUser(User newUser);
