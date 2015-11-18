193b944048ef13ef9899c296ae8427718a8dea42
Emma <emma.schaper@gmail.com>
2015-11-18 15:59:33 -0600

    fix create posts

--- CMS/src/main/webapp/js/createposts.js
+++ CMS/src/main/webapp/js/createposts.js
@@ -14,11 +14,7 @@ $(document).ready(function () {
     $("#add-post-button").click(function (event) {
         var tagstring;
         event.preventDefault();
-        if ($('#add-tags').val() === "") {
-            tagstring = null;
-        } else {
-            tagstring = $('#add-tags').val();
-        }
+
         $.ajax({
             type: 'POST',
             url: 'makePost',
